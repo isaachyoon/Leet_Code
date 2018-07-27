53e8f7ef0e77caaaab45b1f9536c4723597e6912
Isaac Yoon <ysaacioon@yahoo.com>
2018-07-26 20:41:53 -0700

    minWindow

--- minWindow.js
+++ minWindow.js
@@ -1,8 +1,8 @@
 /*
  0123456789012
  ADOBECODEBANC
-    ^
-      ^ 
+          ^
+             ^ 
 
  {
 	A: 1
@@ -12,12 +12,15 @@
 startInd = 0
 endInd = 5
 length: 1
+minLength = 5
 
-minLength = 5;  /(10-3 = 7 nope. keep 5)
 startInd = 0 
 endInd = 5   
 
-
+ 0123456789012
+ ADOBECODEBANC
+    ^
+          ^ 
 */
 function minWindow(s, t) {
 	let letterMap = {};
@@ -28,27 +31,43 @@ function minWindow(s, t) {
 		}
 		letterMap[currLetter]++;
 	}
-	let ptr1, ptr2 = 0;
-	let min = Number.POSITIVE_INFINITY;
-	let startInd, endInd;
-	let lettersToFind = t.length;
+	let ptr1 = ptr2 = 0;
+	let min = Number.POSITIVE_INFINITY, startInd, endInd, lettersToFind = t.length;
 	while (ptr2 !== s.length -1) {
-		while (lettersToFind !== 0) {
+		while (lettersToFind !== 0) { //breaks out if no more letters to find
 			if (ptr2 !== 0 && s[ptr1] === s[ptr2]) {
 				break;
 			}
-			if (--letterMap[s[ptr2++]] === 0) {
-				lettersToFind--;
-				if(lettersToFind === 0 && ((ptr2 - ptr1) < min)) {
-					startInd = ptr1;
-					endInd = ptr2;
-					letterMap[ptr1]++;
-					lettersToFind++;
+			if (!!letterMap[s[ptr2]]) {
+				letterMap[s[ptr2]]--;
+				if (letterMap[s[ptr2]] === 0) {
+					lettersToFind--;
 				}
+ 			}
+			if(lettersToFind === 0 && ((ptr2 - ptr1) < min)) {
+				startInd = ptr1;
+				endInd = ptr2;
+				break;
 			}
+			ptr2++;
 		}
-		while (!letterMap[s[ptr1]]) {
+		// console.log(ptr1, ptr2)
+		if (s[ptr1] !== s[ptr2]) {
+			letterMap[s[ptr1++]]++;
+			lettersToFind++;
+		} else if (s[ptr1] === s[ptr2]) {
+			ptr1++;
+		}
+
+		while (typeof letterMap[s[ptr1]] !== 'number') {
 			ptr1++;
 		}	
 	}
+	console.log(startInd, endInd)
+	return s.slice(startInd, endInd+1);
 }
+
+// let s = 'ADOBECODEBANC';
+// let t = 'ABC' 
+let s = 'A', t = 'A';
+console.log(minWindow(s, t));
