2df1827d5182561f414af8973fd0b278e625d3b5
Isaac Yoon <ysaacioon@yahoo.com>
2018-07-30 20:32:28 -0700

    islandPerimeter

---
+++ islandPerimeter.js
@@ -0,0 +1,21 @@
+var islandPerimeter = function(grid) {
+	let perimeter = 0;
+	for (let row = 0; row < grid.length; row++) {
+		for (let col = 0; col < grid[0].length; col++) {
+			if (grid[row][col] === 1) {
+				if (row === 0 || grid[row-1][col] === 0) {perimeter++;} //up 
+				if (row === grid.length - 1 || grid[row+1][col] === 0) {perimeter++;} //down
+				if (col === 0 || grid[row][col-1] === 0) {perimeter++;} //left
+				if (col === grid[0].length - 1 || grid[row][col+1] === 0) {perimeter++;} //right
+			}
+		}
+	} 
+	return perimeter;
+};
+
+let grid = [[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+let res = islandPerimeter(grid)
+console.log(res)
